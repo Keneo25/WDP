4ad7e90cb63a9093ff3c1efca771607b65040a36
Keneo25 <108327914+Keneo25@users.noreply.github.com>
2022-12-08 18:25:15 +0100

    Update zadaniekt.cpp

--- zadanie 7/zadaniekt.cpp
+++ zadanie 7/zadaniekt.cpp
@@ -73,6 +73,33 @@ int main() {
 }
 */
 
+//ZADANIE 2 zmiana 
+
+string reversedText(string a) {
+    string reversedResult = a;
+    for (int i = 0, j = a.length() - 1; i < a.length(); i++, j--) {
+        reversedResult[i] = a[j];
+    }
+    return reversedResult;
+}
+
+int main()
+{
+    string napis;
+    cout << "Enter some string" << endl;
+    cin >> napis;
+    cout << reversedText(napis);
+}
+
+
+
+
+
+
+
+
+
+
 
 //ZADANIE 3
 
