e1c2ba777f9bb7c2e98b70a394368e6739585255
Keneo25 <108327914+Keneo25@users.noreply.github.com>
2022-12-23 10:26:15 +0100

    Add files via upload

---
+++ zadania 11/proj1/proj1.cpp
@@ -0,0 +1,110 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+// ZADANIE WPROWADZAJACE
+
+//int sumaLiczb(int b) {
+//
+//	if(b < 1) {
+//		cout << endl << "Suma liczb: ";
+//		return 0;
+//	}
+//	cout << b << ",";
+//	return b + sumaLiczb(b - 1);
+//}
+//
+//
+//
+//int main()
+//{
+//	int b,result;
+//	cout << "Podaj liczbe: ";
+//	cin >> b;
+//	result = sumaLiczb(b);
+//	cout << result;
+//}
+//
+
+
+
+//ZADANIE 1 silnia
+//
+//int factorial(int a) {
+//	if (a == 0) {
+//		return 1;
+//	}
+//	cout << a << ", ";
+//	return a * factorial(a - 1);
+//}
+//
+//
+//int main() {
+//	int b, result;
+//	cout << "Podaj liczbe: ";
+//	cin >> b;
+//	result = factorial(b);
+//	cout << "Factorial of " << b << "! " << "is ";
+//	cout << result;
+//
+//}
+
+//ZADANIE 2
+
+//int elementciag(int a) {
+//	if(a == 1){
+//		return 1;
+//	}
+//	else if (a < 0) {
+//		cout << "zle";
+//		return 0;
+//	}
+//
+//	return -a * elementciag(a - 1) + 4;
+//
+//}
+//
+//
+//
+//
+//int main() {
+//	int b, result;
+//	cout << "podaj liczbe: ";
+//	cin >> b;
+//	result = elementciag(b);
+//	cout << result;
+//
+//
+//}
+
+
+//ZADANIE 3 FIBONACCI
+
+//int Fib(int a) {
+//    if (a == 0) {
+//        return 0;
+//    }
+//    else if (a == 1) {
+//       return 1;
+//    }
+//    else if (a < 1) {
+//        return 0;
+//    }
+//    cout << a << ",";
+//    return Fib(a - 1) + Fib(a - 2);
+//}
+//
+//
+//
+//
+//int main() {
+//    int b, result;
+//    cout << "Podaj liczbe: ";
+//    cin >> b;
+//    result = Fib(b);
+//   	cout << endl << "Result: " << result;
+//}
+
+
+
